60f85f85d5b96bef965eed7a1ae1d5dfea780334
fortruce <>
2015-06-22 22:12:44 -0400

    add stabilization for file changes to avoid detecting changes in the middle of a read

--- index.js
+++ index.js
@@ -3,39 +3,70 @@ var assert = require('assert');
 var util = require('util');
 var EventEmitter = require('events').EventEmitter;
 
-function Notifier() {
+function Notifier(filename, options) {
   EventEmitter.call(this);
+  this._filename = filename;
+  this._prevSize = fs.statSync(filename).size;
+  this._stable = 0;
+  this._close = false;
+
+  options = options || {};
+  this._interval = options.interval || 500;
+  this._pollTimes = options.pollTimes || 4;
 }
 
 util.inherits(Notifier, EventEmitter);
 
 Notifier.prototype.close = function() {
-  if (this._handle) {
-    this._handle.close();
+  this._close = true;
+};
+
+Notifier.prototype._notify = function(curSize) {
+  this.emit('change', fs.createReadStream(this._filename, {
+    start: this._prevSize,
+    end: curSize
+  }));
+}
+
+Notifier.prototype._size = function() {
+  try {
+    var stat = fs.statSync(this._filename);
+    if (stat)
+      return stat.size;
+    this.emit('error', 'Failed to stat file.');
+    this._close = true;
+    return undefined;
+  } catch (e) {
+    this.emit('error', e);
+    this._close = true;
+    return undefined;
   }
-};
+}
 
-Notifier.prototype.start = function(filename) {
-  var self = this;
-  var prevSize = fs.statSync(filename).size;
+Notifier.prototype._watch = function() {
+  var curSize = this._size();
+  if (this._close)
+    return;
 
-  this._handle = fs.watchFile(filename,
-    { persistent: true, interval: 5000 },
-    function (event) {
-    if (event.size > prevSize) {
-      self.emit('change', fs.createReadStream(filename, {
-          start: prevSize,
-          end: event.size
-        }));
-        prevSize = event.size;
-    }
-  });
-};
+  if (this._prevSize === curSize) {
+    this._stable++;
+  } else {
+    this._prevSize = curSize;
+    this._stable = 0;
+  }
+
+  if (this._stable >= this._pollTimes) {
+    this._notify(curSize);
+    this._stable = 0;
+  }
+
+  setTimeout(this._watch.bind(this), this._interval);
+}
 
 module.exports = {
   notify: function(filename) {
-            var notifier = new Notifier();
-            notifier.start(filename);
+            var notifier = new Notifier(filename);
+            notifier._watch(filename);
             return notifier;
           }
 };
